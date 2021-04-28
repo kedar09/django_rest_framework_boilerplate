c715cdefc866056415957c3af074618c20e43781
kedar09 <httpskedar@gmail.com>
2021-04-28 21:11:54 +0530

    book & author apis updates

--- django_rest_framework_mysql_crud/book_and_author/apis.py
+++ django_rest_framework_mysql_crud/book_and_author/apis.py
@@ -8,16 +8,18 @@ from .models import Author, Book
 from rest_framework import status
 from rest_framework.parsers import JSONParser
 
-
-@api_view(['GET', 'POST'])
-def add_get_authors(request):
+@api_view(['GET'])
+def get_all_authors(request):
     if request.method == 'GET':
         author_list = Author.objects.all()
         author_list_serializer = AuthorSerializer(author_list, many=True)
         # return JsonResponse({"message": "success", "status": 200, "response": author_list_serializer.data})
-        return Response(author_list_serializer.data)
+        return Response(author_list_serializer.data, status=status.HTTP_200_OK)
 
-    elif request.method == 'POST':
+        
+@api_view(['POST'])
+def add_author(request):
+    if request.method == 'POST':
         add_author_serializer = AuthorSerializer(data=request.data)
         if add_author_serializer.is_valid():
             add_author_serializer.save()
@@ -25,8 +27,8 @@ def add_get_authors(request):
         return Response(add_author_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def get_update_delete_author_by_id(request, pk):
+@api_view(['GET'])
+def get_author_by_id(request, pk):
     try:
         author_details = Author.objects.get(pk=pk)
     except Author.DoesNotExist:
@@ -34,50 +36,67 @@ def get_update_delete_author_by_id(request, pk):
 
     if request.method == 'GET':
         author_details_serializer = AuthorSerializer(author_details)
-        return Response(author_details_serializer.data)
+        return Response(author_details_serializer.data, status=status.HTTP_200_OK)
 
-    elif request.method == 'PUT':
+
+@api_view(['PUT'])
+def update_author_by_id(request, pk):
+    try:
+        author_details = Author.objects.get(pk=pk)
+    except Author.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
         update_author_serializer = AuthorSerializer(
             author_details, data=request.data)
         if update_author_serializer.is_valid():
             update_author_serializer.save()
             return Response(update_author_serializer.data, status=status.HTTP_200_OK)
         return Response(update_author_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        print('called')
+    
+
+@api_view(['DELETE'])
+def delete_author_by_id(request, pk):
+    try:
+        author_details = Author.objects.get(pk=pk)
+    except Author.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
         author_details.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'POST'])
-def add_get_books(request):
+@api_view(['GET'])
+def get_books(request):
     if request.method == 'GET':
         books_and_authors_list = Book.objects.all()
         books_and_authors_list_serializer = BookSerializer(
             books_and_authors_list, many=True)
-        return Response(books_and_authors_list_serializer.data)
+        return Response(books_and_authors_list_serializer.data, status=status.HTTP_200_OK)
 
-    elif request.method == 'POST':
+
+@api_view(['POST'])
+def add_book(request):
+    if request.method == 'POST':
         try:
             author_details = Author.objects.get(pk=request.data['author_id'])
-        
         except Book.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-
+        
         book = {
             'book_name': request.data['book_name'],
             'author_id': author_details
         }
         add_book_serializer = BookSerializer(data=book)
-
         if add_book_serializer.is_valid():
-            print(add_book_serializer.validated_data)
             add_book_serializer.create(validated_data=book)
             return Response(add_book_serializer.data, status=status.HTTP_201_CREATED)
         return Response(add_book_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def get_add_update_book_by_id(request, pk):
+
+@api_view(['GET'])
+def get_book_by_id(request, pk):
     try:
         book_and_author_details = Book.objects.get(pk=pk)
     except Book.DoesNotExist:
@@ -85,15 +104,31 @@ def get_add_update_book_by_id(request, pk):
     if request.method == 'GET':
         book_and_author_details_serializer = BookSerializer(
             book_and_author_details)
-        return Response(book_and_author_details_serializer.data)
+        return Response(book_and_author_details_serializer.data, status=status.HTTP_200_OK)
 
-    elif request.method == 'PUT':
-        update_book_serializer = BookSerializer(book_and_author_details, data=request.data)
+
+@api_view(['PUT'])
+def update_book_by_id(request, pk):
+    try:
+        book_and_author_details = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'PUT':
+        update_book_serializer = BookSerializer(
+            book_and_author_details, data=request.data)
         if update_book_serializer.is_valid():
             update_book_serializer.save()
             return Response(update_book_serializer.data, status=status.HTTP_200_OK)
         return Response(update_book_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+
+@api_view(['DELETE'])
+def delete_book_by_id(request, pk):
+    try:
+        book_and_author_details = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'DELETE':
         book_and_author_details.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
--- django_rest_framework_mysql_crud/book_and_author/urls.py
+++ django_rest_framework_mysql_crud/book_and_author/urls.py
@@ -1,18 +1,17 @@
 # from django.conf.urls import url
 from django.urls import path
-from .apis import add_get_authors, get_update_delete_author_by_id, add_get_books, get_add_update_book_by_id
+from .apis import add_author, get_all_authors, get_author_by_id, update_author_by_id, delete_author_by_id, get_books, add_book, get_book_by_id, update_book_by_id, delete_book_by_id
 
 urlpatterns = [
-    path('getAuthors/', add_get_authors),
-    path('addAuthor/', add_get_authors),
-    path('getAuthorById/<int:pk>/', get_update_delete_author_by_id),
-    path('updateAuthor/<int:pk>/', get_update_delete_author_by_id),
-    path('deleteAuthor/<int:pk>/', get_update_delete_author_by_id),
-    
-    # path('getBooks/', add_get_books),
-    path('getBooksAndAuthors/', add_get_books),
-    path('addBook/', add_get_books),
-    path('getBookAndAuthorById/<int:pk>/', get_add_update_book_by_id),
-    path('updateBook/<int:pk>/', get_add_update_book_by_id),
-    path('deleteBook/<int:pk>/', get_add_update_book_by_id),
+    path('getAllAuthors/', get_all_authors),
+    path('addAuthor/', add_author),
+    path('getAuthorById/<int:pk>/', get_author_by_id),
+    path('updateAuthorById/<int:pk>/', update_author_by_id),
+    path('deleteAuthorById/<int:pk>/', delete_author_by_id),
+
+    path('getAllBooksAndAuthors/', get_books),
+    path('addBook/', add_book),
+    path('getBookAndAuthorById/<int:pk>/', get_book_by_id),
+    path('updateBookById/<int:pk>/', update_book_by_id),
+    path('deleteBookById/<int:pk>/', delete_book_by_id),
 ]
